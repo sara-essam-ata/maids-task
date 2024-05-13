8f68f50c1aee223b95d743977bbf3fdc628f9086
sara-essam-ata <samsarh194@gmail.com>
2024-05-13 13:04:02 +0200

    install packages & style sheet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { NgxSpinnerModule } from "ngx-spinner";
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    NgxSpinnerModule.forRoot({
+      type: 'ball-scale-multiple',
+    }),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
